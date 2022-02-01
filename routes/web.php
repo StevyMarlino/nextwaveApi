517739d16bebee3348f3b0adbf210adb5447ec3f
stevy Endaman <stevyabessolo@gmail.com>
2022-02-01 13:28:39 +0100

    add

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::redirect('/','/documentation');
-
-Route::get('/documentation', function () {
+Route::get('/', function () {
     return view('scribe.index');
 });
